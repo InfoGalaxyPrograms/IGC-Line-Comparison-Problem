7eeb00f6996048043548f02f40a4b5a68d5d9a38
InfoGalaxyPrograms <hiteshsuryawanshi2410@gmail.com>
2022-03-14 10:03:36 +0530

    ADDED: Line Comparison Problem using Java

---
+++ src/com/infogalaxy/LineComparison.java
@@ -0,0 +1,7 @@
+package com.infogalaxy;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Problem developed by InfoGalaxy Computers.");
+    }
+}
