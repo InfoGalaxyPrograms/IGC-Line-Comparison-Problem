001ce7812e67f84878058213077da2beb131b826
InfoGalaxyPrograms <hiteshsuryawanshi2410@gmail.com>
2022-03-14 10:28:42 +0530

    MODIFIED: Check Equality of 2 Lines

--- src/com/infogalaxy/LineComparison.java
+++ src/com/infogalaxy/LineComparison.java
@@ -3,6 +3,15 @@ package com.infogalaxy;
 import java.util.Scanner;
 
 public class LineComparison {
+
+    public static void equals(double lengthOfLine1,double lengthOfLine2) {
+        if(lengthOfLine1 == lengthOfLine2) {
+            System.out.println("Both Lines are Same.");
+        } else {
+            System.out.println("Lines are Not Same.");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Line Comparison Problem developed by InfoGalaxy Computers.");
@@ -11,6 +20,11 @@ public class LineComparison {
         double x2;
         double y1;
         double y2;
+        //UC-2 Length of Line 2 Calculated
+        double a1;
+        double a2;
+        double b1;
+        double b2;
         System.out.println("Enter the Point for X1 and Y1 : ");
         x1 = sc.nextDouble();
         y1 = sc.nextDouble();
@@ -19,5 +33,16 @@ public class LineComparison {
         y2 = sc.nextDouble();
         double lengthOfLine1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         System.out.println("Length of Line 1 : "+ lengthOfLine1);
+
+        System.out.println("Enter the Point for A1 and B1 : ");
+        a1 = sc.nextDouble();
+        b1 = sc.nextDouble();
+        System.out.println("Enter the Point for A2 and B2 : ");
+        a2 = sc.nextDouble();
+        b2 = sc.nextDouble();
+        double lengthOfLine2 = Math.sqrt(Math.pow((a2 - a1), 2) + Math.pow((b2 - b1), 2));
+        System.out.println("Length of Line 2 : "+ lengthOfLine2);
+
+        equals(lengthOfLine1, lengthOfLine2);
     }
 }
